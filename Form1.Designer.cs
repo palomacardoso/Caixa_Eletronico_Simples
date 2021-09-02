a8df13a511ee09be5402bd468c3ce89766e057f7
PalomaCardoso <pallomacardoso64@gmail.com>
2021-09-02 01:45:53 -0300

    Caixa Eletrônico Simples
    Criado durante uma aula na faculdade como desafio

---
+++ Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace CAIXAELETRÔNICO_Paloma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.lbl10 = new System.Windows.Forms.Label();
+            this.lbl25 = new System.Windows.Forms.Label();
+            this.lbl40 = new System.Windows.Forms.Label();
+            this.lbl100 = new System.Windows.Forms.Label();
+            this.lbl500 = new System.Windows.Forms.Label();
+            this.lblNotas = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rBMaiorVariedade = new System.Windows.Forms.RadioButton();
+            this.rBMenorQuantidade = new System.Windows.Forms.RadioButton();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.lblValor = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(383, 161);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(59, 13);
+            this.lbl3.TabIndex = 23;
+            this.lbl3.Text = "Notas de 3";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Location = new System.Drawing.Point(383, 110);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(59, 13);
+            this.lbl4.TabIndex = 22;
+            this.lbl4.Text = "Notas de 4";
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Location = new System.Drawing.Point(289, 161);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(59, 13);
+            this.lbl5.TabIndex = 21;
+            this.lbl5.Text = "Notas de 5";
+            // 
+            // lbl10
+            // 
+            this.lbl10.AutoSize = true;
+            this.lbl10.Location = new System.Drawing.Point(289, 110);
+            this.lbl10.Name = "lbl10";
+            this.lbl10.Size = new System.Drawing.Size(65, 13);
+            this.lbl10.TabIndex = 20;
+            this.lbl10.Text = "Notas de 10";
+            // 
+            // lbl25
+            // 
+            this.lbl25.AutoSize = true;
+            this.lbl25.Location = new System.Drawing.Point(188, 161);
+            this.lbl25.Name = "lbl25";
+            this.lbl25.Size = new System.Drawing.Size(65, 13);
+            this.lbl25.TabIndex = 19;
+            this.lbl25.Text = "Notas de 25";
+            // 
+            // lbl40
+            // 
+            this.lbl40.AutoSize = true;
+            this.lbl40.Location = new System.Drawing.Point(188, 110);
+            this.lbl40.Name = "lbl40";
+            this.lbl40.Size = new System.Drawing.Size(65, 13);
+            this.lbl40.TabIndex = 18;
+            this.lbl40.Text = "Notas de 40";
+            // 
+            // lbl100
+            // 
+            this.lbl100.AutoSize = true;
+            this.lbl100.Location = new System.Drawing.Point(88, 161);
+            this.lbl100.Name = "lbl100";
+            this.lbl100.Size = new System.Drawing.Size(71, 13);
+            this.lbl100.TabIndex = 17;
+            this.lbl100.Text = "Notas de 100";
+            // 
+            // lbl500
+            // 
+            this.lbl500.AutoSize = true;
+            this.lbl500.Location = new System.Drawing.Point(88, 110);
+            this.lbl500.Name = "lbl500";
+            this.lbl500.Size = new System.Drawing.Size(71, 13);
+            this.lbl500.TabIndex = 16;
+            this.lbl500.Text = "Notas de 500";
+            // 
+            // lblNotas
+            // 
+            this.lblNotas.AutoSize = true;
+            this.lblNotas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNotas.Location = new System.Drawing.Point(12, 127);
+            this.lblNotas.Name = "lblNotas";
+            this.lblNotas.Size = new System.Drawing.Size(61, 20);
+            this.lblNotas.TabIndex = 15;
+            this.lblNotas.Text = "Notas:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rBMaiorVariedade);
+            this.groupBox1.Controls.Add(this.rBMenorQuantidade);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 44);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(374, 52);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Qual a Preferência?";
+            // 
+            // rBMaiorVariedade
+            // 
+            this.rBMaiorVariedade.AutoSize = true;
+            this.rBMaiorVariedade.Location = new System.Drawing.Point(241, 21);
+            this.rBMaiorVariedade.Name = "rBMaiorVariedade";
+            this.rBMaiorVariedade.Size = new System.Drawing.Size(127, 20);
+            this.rBMaiorVariedade.TabIndex = 1;
+            this.rBMaiorVariedade.TabStop = true;
+            this.rBMaiorVariedade.Text = "Maior Variedade";
+            this.rBMaiorVariedade.UseVisualStyleBackColor = true;
+            this.rBMaiorVariedade.CheckedChanged += new System.EventHandler(this.rBMaiorVariedade_CheckedChanged);
+            // 
+            // rBMenorQuantidade
+            // 
+            this.rBMenorQuantidade.AutoSize = true;
+            this.rBMenorQuantidade.Location = new System.Drawing.Point(98, 19);
+            this.rBMenorQuantidade.Name = "rBMenorQuantidade";
+            this.rBMenorQuantidade.Size = new System.Drawing.Size(137, 20);
+            this.rBMenorQuantidade.TabIndex = 0;
+            this.rBMenorQuantidade.TabStop = true;
+            this.rBMenorQuantidade.Text = "Menor Quantidade";
+            this.rBMenorQuantidade.UseVisualStyleBackColor = true;
+            this.rBMenorQuantidade.CheckedChanged += new System.EventHandler(this.rBMenorQuantidade_CheckedChanged);
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(226, 10);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(226, 20);
+            this.txtValor.TabIndex = 13;
+            // 
+            // lblValor
+            // 
+            this.lblValor.AutoSize = true;
+            this.lblValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValor.Location = new System.Drawing.Point(12, 9);
+            this.lblValor.Name = "lblValor";
+            this.lblValor.Size = new System.Drawing.Size(208, 18);
+            this.lblValor.TabIndex = 12;
+            this.lblValor.Text = "Informe um Valor para Saque: ";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(392, 62);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(60, 23);
+            this.btnLimpar.TabIndex = 24;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 188);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl10);
+            this.Controls.Add(this.lbl25);
+            this.Controls.Add(this.lbl40);
+            this.Controls.Add(this.lbl100);
+            this.Controls.Add(this.lbl500);
+            this.Controls.Add(this.lblNotas);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.lblValor);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Caixa Eletrônico";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Label lbl10;
+        private System.Windows.Forms.Label lbl25;
+        private System.Windows.Forms.Label lbl40;
+        private System.Windows.Forms.Label lbl100;
+        private System.Windows.Forms.Label lbl500;
+        private System.Windows.Forms.Label lblNotas;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rBMaiorVariedade;
+        private System.Windows.Forms.RadioButton rBMenorQuantidade;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label lblValor;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CAIXAELETRÔNICO_Paloma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void qtdMenor (int valor) // procedimento qtdMenor 
+        {
+            float nota500 = 0, nota100 = 0, nota40 = 0, nota25 = 0, nota10 = 0, nota5 = 0, nota4 = 0, nota3 = 0; //declaração das variaveis
+
+                while (valor >= 500 && valor != 501 && valor != 502) // enquanto o valor for >= 500 e dif. dos terminados em 1 e 2
+                {
+                    valor = valor - 500; // valor =- 500 // o valor vai receber ele mesmo menos 500
+                    nota500++; // e então a nota vai receber +1
+                    lbl500.Text = Convert.ToString(nota500); // e o valor vai ser jogado na label
+                }  // EX se o valor for 1000 ele vai voltar e repetir pois o valor restante é >= 500  
+                while (valor >= 100 && valor != 101 && valor != 102)
+                {
+                    valor = valor - 100;
+                    nota100++;
+                    lbl100.Text = Convert.ToString(nota100);
+                }
+                while (valor >= 40 && valor != 41 && valor != 42)
+                {
+                    valor = valor - 40;
+                    nota40++;
+                    lbl40.Text = Convert.ToString(nota40);
+                }
+                while (valor >= 25 && valor != 26 && valor != 27)
+                {
+                    valor = valor - 25;
+                    nota25++;
+                    lbl25.Text = Convert.ToString(nota25);
+                }
+                while (valor >= 10 && valor != 11 && valor != 12)
+                {
+                    valor = valor - 10;
+                    nota10++;
+                    lbl10.Text = Convert.ToString(nota10);
+                }
+                while (valor >= 5 && valor != 6 && valor != 7)
+                {
+                    valor = valor - 5;
+                    nota5++;
+                    lbl5.Text = Convert.ToString(nota5);
+                }
+                while (valor >= 4 && valor != 6)
+                {
+                    valor = valor - 4;
+                    nota4++;
+                    lbl4.Text = Convert.ToString(nota4);
+                }
+                while (valor >= 3)
+                {
+                    valor = valor - 3;
+                    nota3++;
+                    lbl3.Text = Convert.ToString(nota3);
+                } 
+        }
+
+        private void rBMenorQuantidade_CheckedChanged(object sender, EventArgs e) // procedimento do radio button
+        {
+            if (txtValor.Text != "") //se campo estiver preenchido
+            {
+                if (rBMenorQuantidade.Checked == true) //se o radiobutton estiver checado
+                {
+                    limparNotas(); //limpa as labels 'notas'
+
+                    if (float.Parse(txtValor.Text) < 3) //se o valor for menor que 3
+                    {
+                        MessageBox.Show("Saque Insuficiente. Insira um valor maior que 2!");
+                    }
+                    else //se não
+                    {
+                        qtdMenor(Convert.ToInt32(txtValor.Text)); //ai sim chama a variavel
+                    }
+                }
+            }
+            else //se estiver em branco
+            {
+                MessageBox.Show("Campo em Branco! Preencha-o.");
+            }     
+        }
+
+        void qtdMaior(int valor) // procedimento qtdMaior
+        {
+           int total = 0, nota3 = 0, nota4 = 0, nota5 = 0, nota10 = 0, nota25 = 0, nota40 = 0, nota100 = 0, nota500 = 0; // declaração das variaveis
+           while (total < valor) // enquanto o total (que é inicializado com 0) for menor q o valor do usuário
+            {
+                if (total < valor) // se total for menor que o valor do usuário
+                {
+                    total = total + 3; // então total recebe ele mesmo + 3
+                    nota3++; // a nota de 3 recebe 1
+                }
+                if (total > valor) // se o total (q agr vale 3) for maior que o valor do usuario
+                {
+                    total = total - 3; // então total recebe ele mesmo - 3
+                    nota3--; // e uma nota de 3 é retirada, pois o total não pode ser maior que o valor
+                }
+
+                if (total < valor)
+                {
+                    total = total + 4;
+                    nota4++;
+                }
+                if (total > valor)
+                {
+                    total = total - 4;
+                    nota4--;
+                }
+
+                if (total < valor)
+                {
+                    total = total + 5;
+                    nota5++;
+                }
+                if (total > valor)
+                {
+                    total = total - 5;
+                    nota5--;
+                }
+
+                if (total < valor)
+                {
+                    total = total + 10;
+                    nota10++;
+                }
+                if (total > valor)
+                {
+                    total = total - 10;
+                    nota10--;
+                }
+
+                if (total < valor)
+                {
+                    total = total + 25;
+                    nota25++;
+                }
+                if (total > valor)
+                {
+                    total = total - 25;
+                    nota25--;
+                }
+
+                if (total < valor)
+                {
+                    total = total + 40;
+                    nota40++;
+                }
+                if (total > valor)
+                {
+                    total = total - 40;
+                    nota40--;
+                }
+
+                if (total < valor)
+                {
+                    total = total + 100;
+                    nota100++;
+                }
+                if (total > valor)
+                {
+                    total = total - 100;
+                    nota100--;
+                }
+
+                if (total < valor)
+                {
+                    total = total + 500;
+                    nota500++;
+                }
+                if (total > valor)
+                {
+                    total = total - 500;
+                    nota500--;
+                }
+
+                if (total - valor == -1) // se total (EX = 100) - valor (EX = 101) for igual a -1)
+                {
+                    total = total - 3;  // então total recebe ele mesmo - 3 
+                    total = total + 4; // e total recebe ele + 4
+                    nota3--; // tira a nota de 3
+                    nota4++; // coloca a nota de 4
+                } // isso pra tratar o resto 1, 2, -1, -2
+                if (total - valor == -2)
+                {
+                    total = total - 3;
+                    total = total + 5;
+                    nota5++;
+                    nota3--;
+                }
+                if (total - valor == 2)
+                {
+                    total = total - 5;
+                    total = total + 3;
+                    nota5--;
+                    nota3++;
+                }
+                if (total - valor == 1)
+                {
+                    total = total - 4;
+                    total = total + 3;
+                    nota4--;
+                    nota3++;
+                } 
+            } //se o número já ocupou todas as notas então o while se repete até o número inteiro
+            lbl500.Text = Convert.ToString(nota500);
+            lbl100.Text = Convert.ToString(nota100);
+            lbl40.Text = Convert.ToString(nota40);
+            lbl25.Text = Convert.ToString(nota25);
+            lbl10.Text = Convert.ToString(nota10);
+            lbl5.Text = Convert.ToString(nota5);
+            lbl4.Text = Convert.ToString(nota4);
+            lbl3.Text = Convert.ToString(nota3); // os valores são jogados nas labels
+        }
+
+        private void rBMaiorVariedade_CheckedChanged(object sender, EventArgs e) // procedimento do radio button
+        {
+            if (rBMaiorVariedade.Checked == true) // se o botão estiver checado
+            {
+                limparNotas(); // ele limpa as notas 
+                if (txtValor.Text != "") // se a text estiver preenchida
+                {
+                    if (float.Parse(txtValor.Text) < 3) // se o valor do usuário for menor q 3
+                    {
+                        MessageBox.Show("Saque Insuficiente. Insira um valor maior que 2!");
+                    }
+                    else // se não
+                    {
+                        qtdMaior(Convert.ToInt32(txtValor.Text)); // chama o procedimento
+                    }
+                }
+                else // se não estiver preenchido
+                {
+                    MessageBox.Show("Campo em Branco! Preencha-o.");
+                }   
+            }
+        }
+        void limparNotas() // procedimento que limpa as labels após elas exibirem a qtd de notas
+        {
+            lbl500.Text = "Notas de 500";
+            lbl100.Text = "Notas de 100";
+            lbl40.Text = "Notas de 40";
+            lbl25.Text = "Notas de 25";
+            lbl10.Text = "Notas de 10";
+            lbl5.Text = "Notas de 5";
+            lbl4.Text = "Notas de 4";
+            lbl3.Text = "Notas de 3";
+        }
+        void Limpar() // procedimento que limpa 
+        {
+            rBMenorQuantidade.Checked = false;
+            rBMaiorVariedade.Checked = false; // os dois radio button
+            limparNotas(); // as labels
+            txtValor.Text = ""; // a textbox
+            txtValor.Focus(); // e coloca o foco na text
+        }
+        private void btnLimpar_Click(object sender, EventArgs e) // procedimento do botão limpar
+        {
+            Limpar(); // chama o procedimento limpar
+        }
+    }
+}
